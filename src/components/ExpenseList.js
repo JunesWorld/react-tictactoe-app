aa0fcf61cc1627e97f15a9760881ad52b6aba93a
June <sungj1207@gmail.com>
2023-07-21 15:02:40 +0900

    ExpenseForm, ExpenseList 컴포넌트 생성하기

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './ExpenseList.css'
+
+export class ExpenseList extends Component {
+  render() {
+    return (
+      <>
+        <ul className='list'>
+          {/** Expnese Item */}
+        </ul>
+        <button className='btn'>
+          목록 지우기
+        </button>
+      </>
+    )
+  }
+}
+
+export default ExpenseList
