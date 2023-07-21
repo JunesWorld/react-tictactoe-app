d9b33aaf7de360ff04525125c57b559abada9397
June <sungj1207@gmail.com>
2023-07-21 16:30:06 +0900

    Map 메소드를 사용한 지출 목록 나열

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -11,7 +11,13 @@ export class ExpenseList extends Component {
       <>
         <ul className='list'>
           {/** Expnese Item */}
-          <ExpenseItem />
+          {this.props.initialExpenses.map(expense => {
+            return (
+              <ExpenseItem
+                expense={expense}
+                key={expense.id} />
+            )
+          })}
         </ul>
         <button className='btn'>
           목록 지우기
