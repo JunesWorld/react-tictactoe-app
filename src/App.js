1c564bbee04265b5e59484194e1c157f542fe771
June <sungj1207@gmail.com>
2023-07-23 16:17:20 +0900

    총 비용 계산하기(reduce method)

--- src/App.js
+++ src/App.js
@@ -118,7 +118,13 @@ const App = () => {
         <div style={{ display: 'flex', justifyContent: 'end', marginTop: '1rem' }}>
           <p style={{ fontSize: '2rem' }}>
             총지출:
-            <span>원</span>
+            {/* 총 비용 계산하기 reduce */}
+            <span>
+              {expenses.reduce((acc, curr) => {
+                return (acc += curr.amount);
+              }, 0)}
+              원
+            </span>
           </p>
         </div>
 
